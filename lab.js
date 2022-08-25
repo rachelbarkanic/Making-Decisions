3907e53359ba0b995f87e50e6180faf640ca6e11
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-08-25 11:52:33 -0700

    probelm 5 solved

--- lab.js
+++ lab.js
@@ -88,7 +88,9 @@ if (temperature > 80 && rain) {
   Create a for-loop that runs 10 times, and on each iteration logs the word 'hello'.
 */
 
-//CODE HERE
+for (let i = 0; i < 10; i++) {
+  console.log('Hello')
+}
 
 ////////// PROBLEM 6 //////////
 
